f828ea9dfed4f92b7b61d18cb619bd7349ac699b
Leewell-Muchiri <leewellmacharia10@gmail.com>
2023-04-01 17:36:23 +0300

    completed backend

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,2 +1,63 @@
 class CarsController < ApplicationController
+
+    # before_action :authorize, except: [:index, :show]
+
+    def index
+        cars = Car.all
+        render json: cars, status: :ok
+    end
+
+    def show
+        car = find_car
+
+        if car
+            render json: car, status: :ok
+        else
+            render json: { error: "Car not found" }, state: :not_found
+        end        
+    end
+
+    def create
+        car = Car.create(car_params)
+
+        if car.valid?
+            render json: car, status: :created
+        else
+            render json: { errors: "An error occured. Please try again" }, status: 404
+        end
+    end
+
+    def update
+        car = find_car
+
+        if car
+            car.update(car_params)
+            render json: car, status: :accepted
+        else
+            render json: { error: "Car not found" }, status: :not_found
+        end
+    end
+
+    # def delete
+
+    #     car = find_car
+    #     if car
+    #         car.destroy
+    #         head: no_content
+    #     else
+    #         render json: { error: "Car not found" }, status: :not_found
+    #     end
+    # end
+
+    
+
+    private
+
+    def car_params
+        params.permit(:model, :year_of_manufacture, :transmission, :fuel_type, :price, :user_id,  :front_right_url, :back_left_url, :interior_url)
+    end
+
+    def find_car
+        car = Car.find_by(id: params[:id])
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
+# class User < ApplicationRecord
+#     has_secure_password
+#     validates :password, presence: true
+# end
+
 class User < ApplicationRecord
+    #password encryption   
     has_secure_password
+    #relationships
+    has_many :orders
+    has_many :reviews
+    has_many :cars, through: :reviews
+    #validations
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true, format: {with: URI::MailTo::EMAIL_REGEXP}
+    validates :password, presence: true, length: {:within => 4..20}, if: -> { new_record? || !password.nil?}
 end
