7b07180fdef48fc7766202ab3a7ed99ad4c81c47
Leewell-Muchiri <leewellmacharia10@gmail.com>
2023-04-02 21:39:47 +0300

    deployment

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -4,32 +4,16 @@ class CarsController < ApplicationController
         cars = Car.all
         render json: cars, status: :ok
     end
-<<<<<<< HEAD
     def show
         car = find_car
-=======
-
-    def show
-        car = find_car
-
->>>>>>> 3454c62fefdb54921e806ac555a1ea8aa068071c
         if car
             render json: car, status: :ok
         else
             render json: { error: "Car not found" }, state: :not_found
-<<<<<<< HEAD
         end
     end
     def create
         car = Car.create(car_params)
-=======
-        end        
-    end
-
-    def create
-        car = Car.create(car_params)
-
->>>>>>> 3454c62fefdb54921e806ac555a1ea8aa068071c
         if car.valid?
             render json: car, status: :created
         else
@@ -39,10 +23,6 @@ class CarsController < ApplicationController
 
     def update
         car = find_car
-<<<<<<< HEAD
-=======
-
->>>>>>> 3454c62fefdb54921e806ac555a1ea8aa068071c
         if car
             car.update(car_params)
             render json: car, status: :accepted
@@ -51,7 +31,6 @@ class CarsController < ApplicationController
         end
     end
 
-<<<<<<< HEAD
     def delete
         car = find_car
         if car
@@ -65,27 +44,6 @@ class CarsController < ApplicationController
     def car_params
         params.permit(:model, :year_of_manufacture, :transmission, :fuel_type, :price, :user_id,  :front_right_url, :back_left_url, :interior_url)
     end
-=======
-    # def delete
-
-    #     car = find_car
-    #     if car
-    #         car.destroy
-    #         head: no_content
-    #     else
-    #         render json: { error: "Car not found" }, status: :not_found
-    #     end
-    # end
-
-    
-
-    private
-
-    def car_params
-        params.permit(:model, :year_of_manufacture, :transmission, :fuel_type, :price, :user_id,  :front_right_url, :back_left_url, :interior_url)
-    end
-
->>>>>>> 3454c62fefdb54921e806ac555a1ea8aa068071c
     def find_car
         car = Car.find_by(id: params[:id])
     end
